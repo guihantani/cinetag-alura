821eac2aa779ea9552092321533eed1beb4d773c
Guilherme H. <ghantani@gmail.com>
2024-07-03 16:30:23 -0300

    Project Commit

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+
+function Rodape(){
+    return(
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Guilherme Hantani</h2>
+        </footer>
+    )
+}
+
+export default Rodape
